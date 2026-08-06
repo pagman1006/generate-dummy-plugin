49cb270ce2a0187239a193274a87725ec8c3729f
Pedro Andres Gasca Manrique <andresg1006@gmail.com>
2026-08-06 13:28:59 -0600

    First release

---
+++ src/main/java/io/github/pagman1006/constants/Constants.java
@@ -0,0 +1,26 @@
+package io.github.pagman1006.constants;
+
+public class Constants {
+
+    public static final String MOJO_NAME = "generate";
+    public static final String PACKAGE_PROPERTY = "packageBase";
+    public static final String DEFAULT_PACKAGE = "com.bbva.mzic";
+    public static final String PATH_TEST_JAVA = "src/test/java/";
+    public static final String PACKAGE_UTIL = ".util";
+    public static final String DIR_UTIL = "util/";
+    public static final String CONSTANTS_CLASS = "Constants.java";
+    public static final String UTILS_CLASS = "Utils.java";
+    public static final String FACTORY_CLASS = "Factory.java";
+    public static final String PACKAGE = "package ";
+    public static final String IMPORT_STATIC = "import static ";
+    public static final String IMPORT_CONSTANTS = ".util.Constants.*";
+    public static final String IMPORT_UTILS = ".util.Utils.*";
+    public static final String CONSTANTS_FILE = "constants.txt";
+    public static final String UTILS_FILE = "utils.txt";
+    public static final String FACTORY_FILE = "factory.txt";
+    public static final String DELIMITER = "\n";
+    public static final String SEMICOLON = ";";
+    public static final char DOT = '.';
+
+    private Constants() {}
+}
---
+++ src/main/java/io/github/pagman1006/constants/ConstantsLog.java
@@ -0,0 +1,18 @@
+package io.github.pagman1006.constants;
+
+public class ConstantsLog {
+
+    public static final String LOG_GENERATING_CLASSES = "Generating test classes in: ";
+    public static final String LOG_FAILED_CREATE_TEST_DIRECTORY = "Failed to create test directory: ";
+    public static final String LOG_FAILED_CREATE_CONSTANTS = "Failed to create Constants.java";
+    public static final String LOG_FAILED_CREATE_UTILS = "Failed to create Utils.java";
+    public static final String LOG_FAILED_CREATE_FACTORY = "Failed to create Factory.java";
+    public static final String LOG_CREATED_CONSTANTS = "Constants.java created successfully in ";
+    public static final String LOG_CREATED_UTILS = "Utils.java created successfully in ";
+    public static final String LOG_CREATED_FACTORY = "Factory.java created successfully in ";
+    public static final String LOG_FAILED_CREATE_DIRECTORY = "Failed to create directory: ";
+    public static final String LOG_ERROR_WRITING_FILE = "Error writing to file: ";
+    public static final String LOG_ERROR_READER_FILE = "Error reading resource file: ";
+
+    private ConstantsLog() {}
+}
